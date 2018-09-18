9f280983e110abe7d4fa772f4e304cbe32026215
Hanaa Sadoqi <hsadoqi@gmail.com>
2018-09-18 00:46:48 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,34 @@
+def sort_array_asc(int_array)
+  #return copy of array with integers in ascending order
+  int_array.sort.reverse
+end
+
+def sort_array_desc(int_array)
+  #return copy of array with integers in descending order
+end
+
+def sort_array_char_count(str_array)
+  #return copy of array with strings in ascending order by length
+end
+
+def swap_elements(array)
+  #swaps second and third elements
+end
+
+def reverse_array(int_array)
+  #return copy with elements in reverse order
+end
+
+def kesha_maker(str_array)
+  #replaces third character in each string with a $
+  #use .each method to iterate and build a new array
+end
+
+def find_a(str_array)
+  #return all srings that start with the letter 'a'
+end
+
+def sum_array(int_array)
+  #adds together all integers in array and returns sum
+end
 
