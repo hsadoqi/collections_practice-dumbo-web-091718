e3852c735f53e328cde8bffb8505a4dfb8590662
Hanaa Sadoqi <hsadoqi@gmail.com>
2018-09-18 01:03:12 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -28,7 +28,7 @@ def kesha_maker(str_array)
   #replaces third character in each string with a $
   #use .each method to iterate and build a new array
   str_array.each do |element|
-    element.split.to_a
+    element.split
   end
   str_array
 end
